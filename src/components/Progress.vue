db6cee6c31c00d53e6cb7f394fe94165eb66fa4b
KarolZawisza555 <karolzawisza555@gmail.com>
2023-02-10 19:47:28 +0100

    progress bar added computed value

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,15 +1,37 @@
 <template>
 	<div
-		class="progress my-5"
+		class="progress my-1"
 		role="progressbar"
 		aria-label="Animated striped example"
-		aria-valuenow="75"
-		aria-valuemin="0"
+		aria-valuenow="50"
+		aria-valuemin="10"
 		aria-valuemax="100">
 		<div
 			class="progress-bar progress-bar-striped progress-bar-animated"
-			style="width: 75%"></div>
+			:style="valueProgres"></div>
+	</div>
+	<div
+		class="progress my-1"
+		role="progressbar"
+		aria-label="Basic example"
+		aria-valuenow="0"
+		aria-valuemin="0"
+		aria-valuemax="100">
+		<div
+			class="progress-bar"
+			:style="valueProgres"></div>
 	</div>
 </template>
-<script setup></script>
+<script setup>
+import { reactive, onMounted, computed } from "vue";
+const state = reactive({ progress: 0 });
+onMounted(() => {
+	setInterval(() => {
+		state.progress = (state.progress + 2) % 100;
+	}, 500);
+});
+const valueProgres = computed(() => {
+	return `width: ${state.progress}%`;
+});
+</script>
 <style></style>
